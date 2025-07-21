687ca9f86c1696d16722bc5da2e4dd355f0a08cd
Corentin Vanson <corentinvanson@hotmail.com>
2025-07-21 16:02:22 +0200

    log

--- front/api/game.ts
+++ front/api/game.ts
@@ -1,5 +1,6 @@
 import { GameDto } from "@/heraults-components/Game";
 
 const BFFUrl = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://heraultsdelambert-1037528676095.europe-west1.run.app';
+console.log(process.env.NODE_ENV);
 
 export const getGames = async () => (await fetch(`${BFFUrl}/api/games`)).json() as unknown as GameDto[]
