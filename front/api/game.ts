f98b96d88e6c3419386ba560c32ac0ef9daa351f
Corentin Vanson <corentinvanson@hotmail.com>
2025-07-23 20:21:00 +0200

    connect to vercel

--- front/api/game.ts
+++ front/api/game.ts
@@ -1,6 +1,5 @@
 import { GameDto } from "@/heraults-components/Game";
 
-const BFFUrl = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://heraultsdelambert-1037528676095.europe-west1.run.app';
-console.log(process.env.NODE_ENV);
+const BFFUrl = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://heraultsdelambert-mbx6.vercel.app';
 
 export const getGames = async () => (await fetch(`${BFFUrl}/api/games`)).json() as unknown as GameDto[]
